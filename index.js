7808bfe99a5c6f9dbbbf58cd2c3af78e914ecb0d
Joel Cooper <jodycooper16@gmail.com>
2021-04-14 10:08:12 -0700

    commit tree 1046a1ceb11248c51c197041632454c6dcb74cfd

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Your code goes here
+const text = document.getElementById('text')
+
+document.addEventListener("DOMContentLoaded", function() {
+    // const Text = document.getElementById('text')
+    text.remove('text')
+    let newText = document.createElement('p')
+    newText.id = 'done'
+    newText.innerHTML = 'This is really cool!'
+    document.body.appendChild(newText)
+
+
+
+    
+  }); 
+
+  
